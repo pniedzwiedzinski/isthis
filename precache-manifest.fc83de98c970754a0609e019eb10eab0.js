ce2c9efa6ace7708cc105be0f0b88e5fcca27401
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2018-12-20 09:00:12 +0100

    Updates

---
+++ precache-manifest.fc83de98c970754a0609e019eb10eab0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4979895a9962959d8a4a",
+    "url": "/prd-ev/isthis/static/js/main.4979895a.chunk.js"
+  },
+  {
+    "revision": "cfe3ff27be7944dc3662",
+    "url": "/prd-ev/isthis/static/js/1.cfe3ff27.chunk.js"
+  },
+  {
+    "revision": "56b94ec1d947dcd359c1",
+    "url": "/prd-ev/isthis/static/js/runtime~main.56b94ec1.js"
+  },
+  {
+    "revision": "3ad74378bd55abfecde1c9ca33712442",
+    "url": "/prd-ev/isthis/index.html"
+  }
+];
