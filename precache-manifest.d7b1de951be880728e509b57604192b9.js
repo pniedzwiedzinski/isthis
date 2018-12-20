7fd10b542cdb41dab09a0ac6f1412657b9e2f1c2
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2018-12-20 09:21:34 +0100

    Updates

---
+++ precache-manifest.d7b1de951be880728e509b57604192b9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4979895a9962959d8a4a",
+    "url": "/isthis/static/js/main.4979895a.chunk.js"
+  },
+  {
+    "revision": "7fc646289cfa45117bd4",
+    "url": "/isthis/static/js/1.7fc64628.chunk.js"
+  },
+  {
+    "revision": "fa9dd93686a0ab6e25ea",
+    "url": "/isthis/static/js/runtime~main.fa9dd936.js"
+  },
+  {
+    "revision": "dc5520522203a2d28668c3c7fec94482",
+    "url": "/isthis/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/prd-ev/isthis/precache-manifest.fc83de98c970754a0609e019eb10eab0.js"
+  "/isthis/precache-manifest.d7b1de951be880728e509b57604192b9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/prd-ev/isthis/index.html", {
+workbox.routing.registerNavigationRoute("/isthis/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
